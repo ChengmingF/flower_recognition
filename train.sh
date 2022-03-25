97a4961abb6bbc90dfda0d339ba36024451c0b47
ChengmingF <98319733+ChengmingF@users.noreply.github.com>
2022-03-25 10:01:40 +0100

    Delete train.sh

--- train.sh
+++
@@ -1,9 +0,0 @@
-for net in densenet161 resnet18 mobilenet_v2
-do
-    for new_fc in True False
-    do
-        python train.py --network $net --$new_fc
-    done
-done
-
-python train.py --network vgg16
